216555981ee0d706e9428d547459a7ff2b0f2406
FrancisFlow <iamngigikaranja1@aol.com>
2021-12-14 23:01:42 +0300

    Imported render template, set up config in __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
 
+from config import DevConfig
+
 #initialization of application
 
-
 app= Flask(__name__)
+
+
+#configuration set up
+app.config.from_object(DevConfig)
 
 from app import views
