5a76f392f9ab23adc878b209442dba890426a9da
FrancisFlow <iamngigikaranja1@aol.com>
2021-12-16 08:33:53 +0300

    Moved the view and error files to main and created an __init__.py file

---
+++ app/main/__init__.py
@@ -0,0 +1,26 @@
+from flask import Blueprint
+from config import config_options
+
+from flask import Flask
+from flask_bootstrap import Bootstrap, bootstrap_find_resource
+main = Blueprint('main', __name__)
+from . import views, errors
+
+
+def create_app(config_name):
+   app = Flask(__name__)
+
+   # creating app configurations
+
+   app.config.from_object(config_options[config_name])
+
+   # Initializing flask extensions
+
+   bootstrap_find_resource.init_app(app)
+
+   # registering the blueprint
+
+   from . import main as main_blueprint
+   app.register_blueprint(main_blueprint)
+
+   return app
---
+++ app/main/views.py
@@ -0,0 +1,15 @@
+from flask import render_template, request, redirect, url_for
+from ..requests import get_sources
+from models import Source,Articles
+from app import app
+
+@app.route("/")
+def index():
+    """
+    view root page function
+    """
+
+    #getting sources
+    tech_sources = get_sources('technology')
+    print(tech_sources)
+    return render_template('index.html', technology=tech_sources)
