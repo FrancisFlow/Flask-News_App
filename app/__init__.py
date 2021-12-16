46df32d6502d4d8ec5bd1303be852c66b6ad7eb2
FrancisFlow <iamngigikaranja1@aol.com>
2021-12-16 08:33:08 +0300

    Created and added Blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -4,15 +4,23 @@ from config import config_options
 
 bootstrap = Bootstrap()
 
-#initialization of application
+def create_app(config_name):
 
-app= Flask(__name__, instance_relative_config = True)
+     app= Flask(__name__, instance_relative_config = True)
 
-# Creating the app configurations
-app.config.from_object(config_options[config_name])
+   # Creating the app configurations
+     app.config.from_object(config_options[config_name])
 
-#initializing bootstrap extension
-bootstrap.init_app(app)
+    #initializing bootstrap extension
+     bootstrap.init_app(app)
+
+  #Registering the blueprint
+     from .main import main as main_blueprint
+     app.register_blueprint(main_blueprint)
+
+   #setting config
+     from requests import configure_request
+     configure_request(app)
 
 
-from .main import views
+     return app
