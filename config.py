5bf80185ea880736b4bfbe22c6f32a330fbe3e52
FrancisFlow <iamngigikaranja1@aol.com>
2022-01-09 15:30:34 +0300

    Imported os and added config options

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import os
 class Config:
 
     """
@@ -5,6 +6,10 @@ class Config:
     
     """
 
+    NEWS_SOURCES_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 class ProdConfig(Config):
 
@@ -12,9 +17,6 @@ class ProdConfig(Config):
 
 class DevConfig(Config):
     DEBUG = True
-
-
-
 
 config_options = {
 'development':DevConfig,
