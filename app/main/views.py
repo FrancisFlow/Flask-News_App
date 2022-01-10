7c50554fb64d1a21b066abae1b4cbf8e2e00493a
FrancisFlow <iamngigikaranja1@aol.com>
2022-01-10 11:38:26 +0300

    Created articles view function

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,9 @@
 from flask import render_template, request, redirect, url_for
-from ..requests import get_sources
+from ..requests import get_sources, get_article
 from ..models import Source,Articles
 from . import main
 
-@main.route("/")
+@main.route('/', methods = ['GET', 'POST'])
 def index():
     """
     view root page function
@@ -11,5 +11,15 @@ def index():
 
     #getting sources
     tech_sources = get_sources('technology')
-    print(tech_sources)
-    return render_template('index.html', technology=tech_sources)
+    title = 'News Flow'
+    return render_template('index.html', title = title, technology=tech_sources)
+
+
+@main.route('/source/<id>', ['GET', 'POST'])
+def articles(id):
+    """
+    function to return articles
+    """
+    articles = get_article(id)
+    title = f'Headline {id}'
+    return render_template('article.html', title = title, articles = articles)
